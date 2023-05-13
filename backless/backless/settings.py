9cacbad1c7cd88bc3ad1c1e7a709d2c1e8d48ecf
Marconi Soares <marconi.developer@outlook.com.br>
2023-05-13 00:14:45 -0300

    feat(backless): connect to aws s3

--- backless/backless/settings.py
+++ backless/backless/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
 
     # Backless
     'main_site.apps.MainSiteConfig',
+
+    # S3
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -92,6 +95,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = os.getenv('DEFAULT_FILE_STORAGE')
 
 
 # Default primary key field type
@@ -103,3 +109,12 @@ PROJECT_NAME = os.getenv('PROJECT_NAME')
 QUEUE_REGION = os.getenv('QUEUE_REGION')
 QUEUE_ID = os.getenv('QUEUE_ID')
 WORKER_URL = os.getenv('WORKER_URL')
+
+
+# S3
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = True
+AWS_DEFAULT_ACL = None
+
