3ecbf980745cf65be50fc8e739b1001899980337
Marconi Soares <marconi.developer@outlook.com.br>
2023-05-18 18:39:31 -0300

    style(main_site): add styling for wait_room and some styling changes for index

--- backless/main_site/static/js/websocket.js
+++ backless/main_site/static/js/websocket.js
@@ -1,26 +1,38 @@
+const ws = new WebSocket(wsUrl)
+const inform = document.querySelector("#inform-h2")
+const image = document.querySelector("#img")
+
 function handleTaskCompleted(msg) {
   msg = msg.split(" ")
   const image_url = msg[0]
   const image_name = msg[1]
   
   inform.innerHTML = "Clique em <i>dowload</i> para baixar sua imagem"
+  inform.className = "completed"
   image.src = image_url
-  
-  const dowloadLink = createDowloadLink()
+
+  const goBackLink = createLink("voltar", "goBackLink")
+  goBackLink.href = "/"
+
+  const dowloadLink = createLink("dowload", "downloadLink")
   dowloadLink.href = image_url
   dowloadLink.download = image_name
+
   show()
 }
 
 function handleTaskReceived() {
   inform.innerText = "Sua imagem está sendo processada."
+  inform.className = "received"
 }
 
-function createDowloadLink() {
-  const dowloadLink = document.createElement("a")
-  dowloadLink.innerText = "dowload"
-  document.body.appendChild(dowloadLink)
-  return dowloadLink
+function createLink(innerText, id) {
+  const container = document.querySelector("#links-container")
+  const link = document.createElement("a")
+  link.innerText = innerText
+  link.id = id
+  container.appendChild(link)
+  return link
 }
 
 function show() {
