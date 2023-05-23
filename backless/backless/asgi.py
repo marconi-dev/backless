4d63a196b945f7fa8ed229dcf1d4aa38d1ac6bee
Marconi Soares <marconi.developer@outlook.com.br>
2023-05-23 00:22:21 -0300

    fix(main_site/asgi): setup django before importing websocket_urls

--- backless/backless/asgi.py
+++ backless/backless/asgi.py
@@ -1,12 +1,15 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
-from main_site.routing import websocket_urls
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backless.settings')
+django.setup()
+
+from main_site.routing import websocket_urls
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
